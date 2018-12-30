b60702fa570806c95559a6d4ad413f5cf899f984
Jon Ribeiro <contact@jonathas.com>
2018-12-31 00:52:24 +0100

    Improved how bootstrap is being called

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -1,5 +1,7 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
 import { Negociacao } from './domain/index.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const controller = new NegociacaoController();
 const negociacao = new Negociacao(new Date(), 1, 200);
@@ -8,10 +10,10 @@ headers.set('Content-Type', 'application/json');
 const body = JSON.stringify(negociacao);
 const method = 'POST';
 
-const config = { 
+const config = {
     method,
     headers,
-    body 
+    body
 };
 
 fetch('/negociacoes', config)
